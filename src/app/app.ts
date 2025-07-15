bbc6926e52d8c1c314edc907ff0ae8989059227d
MrMoriarty04 <ramialjaber2004@gmail.com>
2025-07-15 21:12:17 +0300

    finished

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,50 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CurrencyPipe,JsonPipe,DatePipe,TitleCasePipe,DecimalPipe} from '@angular/common';
+import { ConvertPipe } from './convert-pipe';
+
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,  TitleCasePipe , DatePipe , CurrencyPipe ,JsonPipe ,DecimalPipe, ConvertPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pipes');
+  
+  name!:string;
+  date!:string;
+  amount!:number;
+  miles!:number;
+
+  onNameChange(event:Event){
+    const input = event.target as HTMLInputElement;
+    const value = input.value;
+
+    this.name=value;
+  }
+
+    onDateChange(event:Event){
+    const input = event.target as HTMLInputElement;
+    const value = input.value;
+
+    this.date=value;
+  }
+
+  onAmountChange(event:Event){
+    const input = event.target as HTMLInputElement;
+    const value = parseInt(input.value);
+
+    this.amount=value;
+  }
+
+  onMilesChange(event:Event){
+    const input = event.target as HTMLInputElement;
+    const value = parseFloat(input.value);
+
+    this.miles=value;
+  }
+
 }
